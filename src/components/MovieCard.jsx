152b6e8d81915a1dd7832574fc035b612badbf8b
lucasdias0812 <99883219+lucasdias0812@users.noreply.github.com>
2024-10-29 20:57:55 -0300

    Add files via upload

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,27 @@
 import { Link } from "react-router-dom";
 
-export default function MovieCard({ id, title, poster_path, backdrop_path }) {
+export default function MovieCard({ id, title, poster_path }) {
+
+    const handleFavorito = ({id, title, poster_path}) => {
+        let favoritos = JSON.parse(localStorage.getItem('favoritos')) || []
+
+        const isFavorite = favoritos.some(filme => filme.id === id)
+
+        if(isFavorite){
+            favoritos = favoritos.filter(filme => filme.id != id)
+        }
+        else{
+            favoritos.push({id, title, poster_path})
+        }
+
+        localStorage.setItem('favoritos',JSON.stringify(favoritos))
+    }
+    
     return (
-        <div>
+        <div className="flex flex-col text-center">
             <img src={`https://image.tmdb.org/t/p/w154${poster_path}`} />
             <Link to={`/movies/${id}`}>Saber mais</Link>
+            <button onClick={() => handleFavorito({id, title, poster_path})}>Adicionar Favorito</button>
         </div>
     )
 
